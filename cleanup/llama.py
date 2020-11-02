d14b1370fce112cea5e34a00c26e8cf4a0b7b454
Jake Hickenlooper <jake@weboftomorrow.com>
2020-11-02 06:48:44 -0700

    Split template and move parameters.json
    The parameters.json file is better suited to be in the project's repo.
    Cleanup lambda has been updated to get blue and green version info from
    that file now.

--- cleanup/llama.py
+++ cleanup/llama.py
@@ -10,7 +10,6 @@ import boto3
 logger = logging.getLogger()
 logger.setLevel(os.environ.get("LOGGING_LEVEL", logging.DEBUG))
 
-codepipeline = boto3.client('codepipeline')
 
 def get_user_params(job_data):
     ""
@@ -21,15 +20,37 @@ def get_user_params(job_data):
         raise Exception("UserParameters could not be decoded as JSON. {}".format(e))
     return decoded_params
 
-def put_job_success(job_id, message):
+def get_source_params(s3, job_data):
+    """
+    """
+    try:
+        parametersJSONArtifact = list(filter(lambda item: item["name"] == "parametersJSON", job_data["inputArtifacts"])).pop()
+    except Exception as err:
+        logger.error(f"parametersJSON input artifact not found in job data. {job_data}")
+        raise err
+
+    s3Loc = parametersJSONArtifact["location"]["s3Location"]
+    args = {
+        "Bucket": s3Loc["bucketName"],
+        "Key": s3Loc["objectKey"]
+    }
+
+    get_response = s3.get_object(**args)
+    logger.debug(f"S3 get object response: {get_response}")
+    parameters = json.load(get_response["Body"])
+    logger.debug(f"response body: {parameters}")
+
+    return dict(map(lambda x: [x["ParameterKey"], x["ParameterValue"]], parameters))
+
+def put_job_success(codepipeline, job_id, message):
     logger.debug(message)
     try:
         codepipeline.put_job_success_result(
             jobId=job_id)
     except Exception as err:
-        put_job_failure(job_id, f"Exception with put_job_success() with message: '{message}'. {err}")
+        put_job_failure(codepipeline, job_id, f"Exception with put_job_success() with message: '{message}'. {err}")
 
-def put_job_failure(job_id, message):
+def put_job_failure(codepipeline, job_id, message):
     logger.error(message)
     codepipeline.put_job_failure_result(
         jobId=job_id,
@@ -38,9 +59,7 @@ def put_job_failure(job_id, message):
             'type': 'JobFailed'
         })
 
-def list_objects_at_prefix(bucket_name, prefix, continuation_token=None, object_list=[]):
-    s3 = boto3.client('s3')
-
+def list_objects_at_prefix(s3, bucket_name, prefix, continuation_token=None, object_list=[]):
     args = {
         "Bucket": bucket_name,
         "Prefix": prefix
@@ -54,13 +73,11 @@ def list_objects_at_prefix(bucket_name, prefix, continuation_token=None, object_
 
     objects = list(map(lambda x: x['Key'], list_objects_response['Contents']))
     if list_objects_response.get('IsTruncated'):
-        objects = objects + list_objects_at_prefix(bucket_name, prefix, continuation_token=list_objects_response.NextContinuationToken, object_list=objects)
+        objects = objects + list_objects_at_prefix(s3, bucket_name, prefix, continuation_token=list_objects_response.NextContinuationToken, object_list=objects)
     return objects
 
-def delete_files(bucket_name, prefix, continuation_token=None):
+def delete_files(s3, bucket_name, prefix, continuation_token=None):
     "List and delete objects from s3. Limited to 1000 objects at a time."
-    s3 = boto3.client('s3')
-
     args = {
         "Bucket": bucket_name,
         "Prefix": prefix
@@ -85,10 +102,10 @@ def delete_files(bucket_name, prefix, continuation_token=None):
     )
     if response['ResponseMetadata']['HTTPStatusCode'] != 200:
         logger.error('failed. {response}')
-        put_job_failure(job_id, f"Failed to delete files. {response}")
+        put_job_failure(codepipeline, job_id, f"Failed to delete files. {response}")
     if list_objects_response.get('IsTruncated'):
         logger.info("More than 1000 objects to delete. Deleting next chunk.")
-        delete_files(bucket_name, prefix, continuation_token=list_objects_response.NextContinuationToken)
+        delete_files(s3, bucket_name, prefix, continuation_token=list_objects_response.NextContinuationToken)
 
 def find_prefix_not_in_set(common_path, keep_set, keys):
     "Given a list of s3 object keys return the prefix that is not in the keep_set."
@@ -96,6 +113,8 @@ def find_prefix_not_in_set(common_path, keep_set, keys):
 
 def handler(event, context):
     #"https://docs.aws.amazon.com/codepipeline/latest/userguide/actions-invoke-lambda-function.html"
+    s3 = boto3.client('s3')
+    codepipeline = boto3.client('codepipeline')
 
     try:
         job_id = event["CodePipeline.job"]["id"]
@@ -104,14 +123,20 @@ def handler(event, context):
         logger.debug(f"Job data: {job_data}")
 
         user_params = get_user_params(job_data)
+
+        # TODO: get the input artifact for the parametersJSON
+        source_params = get_source_params(s3, job_data)
+        logger.debug(f"source parameters: {source_params}")
+
         logger.debug(f"user parameters: {user_params}")
 
-        keys = list_objects_at_prefix(user_params["StaticSiteFiles"], 'weboftomorrow/production/')
-        prefix_to_delete = find_prefix_not_in_set('weboftomorrow/production/', set([user_params.get('BlueVersion'), user_params.get('GreenVersion')]), keys)
+        production_prefix = '{ProjectSlug}/production/'.format(**source_params)
+        keys = list_objects_at_prefix(s3, user_params["StaticSiteFiles"], production_prefix)
+        prefix_to_delete = find_prefix_not_in_set(production_prefix, set([source_params.get('BlueVersion'), source_params.get('GreenVersion')]), keys)
         for old_prefix in prefix_to_delete:
-            delete_files(user_params["StaticSiteFiles"], 'weboftomorrow/production/' + old_prefix)
+            delete_files(s3, user_params["StaticSiteFiles"], production_prefix + old_prefix)
 
-        put_job_success(job_id, "Complete")
+        put_job_success(codepipeline, job_id, "Complete")
     except Exception as e:
-        put_job_failure(job_id, f"Function failed due to exception. {e}")
+        put_job_failure(codepipeline, job_id, f"Function failed due to exception. {e}")
 
